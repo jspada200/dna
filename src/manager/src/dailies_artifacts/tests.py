cfe56e76034914cdf28b9dc4747f638806f5af1b
jspadafora <jspadafora@ilm.com>
2025-08-21 14:32:48 -0700

    tests

--- src/manager/src/dailies_artifacts/tests.py
+++ src/manager/src/dailies_artifacts/tests.py
@@ -10,9 +10,31 @@ class ArtifactViewSetTests(APITestCase):
         assert response.status_code == status.HTTP_200_OK
 
     def test_artifact_create(self):
+        # First create a Schedule so we have somthing to link to.
+        url = reverse("schedule-list")
+        response = self.client.post(
+            url,
+            {"status": "pending", 
+             "starttime": "2023-01-01T00:00:00Z",
+             "endtime": "2023-01-01T01:00:00Z",
+             "metadata": "{}",
+             "playlistid": "123",
+             "meetingtype": "meet"
+             }, format="json"
+        )
+        assert response.status_code == status.HTTP_201_CREATED
+        schedule_id = response.data["id"]
+
         url = reverse("artifact-list")
-        data = {"name": "Test Artifact", "description": "This is a test artifact."}
+        data = {
+            "transcript": "{}",
+            "context": "{}",
+            "timestamp": "2023-01-01T00:00:00Z",
+            "artifact_link": schedule_id
+        }
         response = self.client.post(url, data, format="json")
         assert response.status_code == status.HTTP_201_CREATED
-        assert response.data["name"] == data["name"]
-        assert response.data["description"] == data["description"]
+        assert response.data["transcript"] == data["transcript"]
+        assert response.data["context"] == data["context"]
+        assert response.data["timestamp"] == data["timestamp"]
+        assert response.data["artifact_link"] == data["artifact_link"]
