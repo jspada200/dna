70c62bf83c4363ec10344a3dc852c21487ca3858
jspadafora <jspadafora@ilm.com>
2025-08-21 15:10:38 -0700

    remove dotenv package since it is loaded in the docker compose files

--- src/manager/src/core/settings.py
+++ src/manager/src/core/settings.py
@@ -14,10 +14,6 @@ import os
 from pathlib import Path
 
 import dj_database_url
-import dotenv
-
-# Load environment variables from .env file
-dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
